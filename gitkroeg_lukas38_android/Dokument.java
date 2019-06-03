25de5191aed8f86ae34a9e2ec9df3e017c8be835
Simon Kroeg <simon.kroeg@icloud.com>
2019-06-03 17:21:17 +0200

    secondtry

--- gitkroeg_lukas38_android/Dokument.java
+++ gitkroeg_lukas38_android/Dokument.java
@@ -1,6 +1,8 @@
 dummy file
 <<<<<<< HEAD
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {
+	if(lukas == intelligent)return false;
+ }
 =======
 public gitdid911(){}
 >>>>>>> 14259e2f4aba82eee8804f4bc75938a033f60867
