3c327ff3a27aba639e7ed8d17da81d6ec04f06f3
Lukas Rogl <l.rogl@gmx.at>
2019-06-03 18:00:06 +0200

    icharbeiteweitermalwieder

--- gitkroeg_lukas38_android/Dokument.java
+++ gitkroeg_lukas38_android/Dokument.java
@@ -5,8 +5,8 @@ public void thisIsANewMethod() {
 	if(lukas == intelligent)return false;
 	Aus Praxis der Gipfeltreffen der Staats- und Regierungschefs hat sich der Europäische Rat entwickelt, ...
 	while(simon != pretty){
-	
+
  }
 =======
-public gitdid911(){}
+public gitdid911(){Falls die Union zusammenbricht, wird die Logik ihres Zerfalls eher der Logik eines Bankenruns als der einer Revolution aehneln.}
 >>>>>>> 14259e2f4aba82eee8804f4bc75938a033f60867
