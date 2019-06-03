36c437f78a6238add64832c392891bab372da618
Lukas Rogl <l.rogl@gmx.at>
2019-06-03 17:17:30 +0200

    newtext

--- gitkroeg_lukas38_android/Dokument.java
+++ gitkroeg_lukas38_android/Dokument.java
@@ -1,6 +1,6 @@
 dummy file
 <<<<<<< HEAD
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {Aus Praxis der Gipfeltreffen der Staats- und Regierungschefs hat sich der Europäische Rat entwickelt, ...}
 =======
 public gitdid911(){}
 >>>>>>> 14259e2f4aba82eee8804f4bc75938a033f60867
